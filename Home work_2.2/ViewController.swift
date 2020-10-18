2232c87bdefda8787d780dc5543520f39d91a430
Pavel Anpleenko <19paul91@mail.ru>
2020-10-18 16:39:37 +0300

    Initial Commit

---
+++ Home work_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work_2.2
+//
+//  Created by Pavel Anpleenko on 18.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
